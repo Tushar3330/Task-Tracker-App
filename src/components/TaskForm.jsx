dd256bd141b8a9574905d9cec3ed3c0664cf929d
Tushar3330 <tusharrana2505@gmail.com>
2025-06-14 12:25:51 +0530

    Made Task Form

--- src/components/TaskForm.jsx
+++ src/components/TaskForm.jsx
@@ -1,5 +1,42 @@
+import { useState } from 'react';
 
-export const TaskForm = () => {
+function TaskForm({ addTask }) {
+  const [title, setTitle] = useState('');
+  const [description, setDescription] = useState('');
 
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    addTask(title, description);
+    setTitle('');
+    setDescription('');
+
+  };
+
+  return (
+    <form onSubmit={handleSubmit} className="mb-6 flex flex-col gap-4">
+      <input
+        type="text"
+        placeholder="Task Title"
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
+        className="p-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+      />
+    
+      <textarea
+        placeholder="Task Description"
+        value={description}
+        onChange={(e) => setDescription(e.target.value)}
+        className="p-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 resize-y"
+      />
+      <button
+        type="submit"
+        className="p-2 bg-green-600 text-white rounded-md hover:bg-green-700"
+      >
+        Add Task
+      </button>
+    </form>
+  );
 }
+
+export default TaskForm;
