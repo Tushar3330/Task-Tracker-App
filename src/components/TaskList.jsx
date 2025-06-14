f72445ee9b1042dc5ef4a8c3ec895120ba5205b2
Tushar3330 <tusharrana2505@gmail.com>
2025-06-14 12:26:50 +0530

    Added Task List to veiw each Task Created

--- src/components/TaskList.jsx
+++ src/components/TaskList.jsx
@@ -1,2 +1,22 @@
-export const TaskList = () => {
+import TaskItem from './TaskItem';
+
+function TaskList({ tasks, toggleTask, deleteTask }) {
+  return (
+    <div>
+      {tasks.length === 0 ? (
+        <p className="text-gray-500 text-center">No tasks found.</p>
+      ) : (
+        tasks.map((task) => (
+          <TaskItem
+            key={task.id}
+            task={task}
+            toggleTask={toggleTask}
+            deleteTask={deleteTask}
+          />
+        ))
+      )}
+    </div>
+  );
 }
+
+export default TaskList;
