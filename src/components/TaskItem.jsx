763e84608f9e3e8f184f90747f20598a5facf47e
Tushar3330 <tusharrana2505@gmail.com>
2025-06-14 12:27:41 +0530

    Created Each Task Template

--- src/components/TaskItem.jsx
+++ src/components/TaskItem.jsx
@@ -1,2 +1,29 @@
-export const TaskItem = () => {
+function TaskItem({ task, toggleTask, deleteTask }) {
+  return (
+    <div
+      className={`flex items-center p-4 border-b ${
+        task.completed ? 'line-through text-gray-500' : ''
+      }`}
+    >
+      <input
+        type="checkbox"
+        checked={task.completed}
+        onChange={() => toggleTask(task.id)}
+        className="mr-4 h-5 w-5"
+      />
+      <div className="flex-1">
+        <h4 className="text-lg font-medium">{task.title}</h4>
+        <p className="text-gray-600">{task.description}</p>
+
+      </div>
+      <button
+        onClick={() => deleteTask(task.id)}
+        className="p-2 bg-red-600 text-white rounded-md hover:bg-red-700"
+      >
+        Delete
+      </button>
+    </div>
+  );
 }
+
+export default TaskItem;
